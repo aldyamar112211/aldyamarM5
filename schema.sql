5dfc22f2b95b79d429b79106091ec6019e1703e3
aldyamar112211 <aldyamaralfirdaus@gmail.com>
2025-05-17 00:31:23 +0700

    modif

--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS mahasiswa;
 
@@ -16,29 +14,3 @@ CREATE TABLE mahasiswa (
 );
 
 INSERT INTO users (username, password) VALUES ('Aldy Amar Al Firdaus', '1');
-=======
--- Hapus tabel jika sudah ada sebelumnya
-=======
->>>>>>> a43d43a (update lagi)
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS mahasiswa;
-
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    username TEXT UNIQUE NOT NULL,        
-    password TEXT NOT NULL                
-);
-
-CREATE TABLE mahasiswa (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    nama TEXT NOT NULL,                   
-    nim TEXT UNIQUE NOT NULL              
-);
-
-<<<<<<< HEAD
--- Menambahkan data awal ke tabel 'users'
-INSERT INTO users (username, password) VALUES ('Aldy Amar Al Firdaus', '1');
->>>>>>> ad90c39 (update)
-=======
-INSERT INTO users (username, password) VALUES ('Aldy Amar Al Firdaus', '1');
->>>>>>> a43d43a (update lagi)
