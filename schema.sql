03f76d84c5bb0948ea490d8ae351e4a8e0ed3483
aldyamar112211 <aldyamaralfirdaus@gmail.com>
2025-05-17 00:05:01 +0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,20 +1,16 @@
--- Hapus tabel jika sudah ada sebelumnya
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS mahasiswa;
 
--- Membuat tabel 'users' untuk menyimpan informasi pengguna
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, -- ID unik yang bertambah otomatis
-    username TEXT UNIQUE NOT NULL,        -- Nama pengguna unik, tidak boleh kosong
-    password TEXT NOT NULL                 -- Kata sandi tidak boleh kosong
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    username TEXT UNIQUE NOT NULL,        
+    password TEXT NOT NULL                
 );
 
--- Membuat tabel 'mahasiswa' untuk menyimpan informasi mahasiswa
 CREATE TABLE mahasiswa (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, -- ID unik yang bertambah otomatis
-    nama TEXT NOT NULL,                   -- Nama mahasiswa, tidak boleh kosong
-    nim TEXT UNIQUE NOT NULL              -- NIM unik, tidak boleh kosong
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    nama TEXT NOT NULL,                   
+    nim TEXT UNIQUE NOT NULL              
 );
 
--- Menambahkan data awal ke tabel 'users'
 INSERT INTO users (username, password) VALUES ('Aldy Amar Al Firdaus', '1');
